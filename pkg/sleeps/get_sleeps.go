8b48fd3952485fa94056df7d87f2f4e9aa8a3d14
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-12-15 17:41:01 +0700

    feat: add pagination to sleeps

--- pkg/sleeps/get_sleeps.go
+++ pkg/sleeps/get_sleeps.go
@@ -1,33 +1,79 @@
 package sleeps
 
 import (
+	"math"
 	"net/http"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rizquadnan/daily-sleep-tracker-api/pkg/common/models"
+	"gorm.io/gorm"
 )
+
+type Pagination struct {
+	getTotalPage func(totalRows int64) int
+	totalRows    int64
+}
+
+func SetupPagination(c *gin.Context, h handler) (func(db *gorm.DB) *gorm.DB, Pagination) {
+	if c.Query("page") == "" || c.Query("page_size") == "" {
+		return func(db *gorm.DB) *gorm.DB {
+			return db
+		}, Pagination{totalRows: 0}
+	}
+
+	page, _ := strconv.Atoi(c.Query("page"))
+	if page == 0 {
+		page = 1
+	}
+
+	pageSize, _ := strconv.Atoi(c.Query("page_size"))
+	switch {
+	case pageSize > 100:
+		pageSize = 100
+	case pageSize <= 0:
+		pageSize = 5
+	}
+
+	offset := (page - 1) * pageSize
+
+	paginator := func(db *gorm.DB) *gorm.DB {
+		return db.Offset(offset).Limit(pageSize)
+	}
+
+	return paginator, Pagination{getTotalPage: func(totalRows int64) int {
+		return int(math.Ceil(float64(totalRows) / float64(pageSize)))
+	}, totalRows: 0}
+}
 
 func (h handler) GetSleeps(c *gin.Context) {
 	var sleeps []models.Sleep
 	userId := c.Query("user")
 
-	if (userId == "") {
-		if result := h.DB.Find(&sleeps); result.Error != nil {
+	paginator, pagination := SetupPagination(c, h)
+
+	if userId == "" {
+		h.DB.Model(models.Sleep{}).Count(&pagination.totalRows)
+		if result := h.DB.Scopes(paginator).Find(&sleeps); result.Error != nil {
 			c.AbortWithError(http.StatusNotFound, result.Error)
 			return
 		}
-	}	else {
-		userIdInt, err := strconv.Atoi(userId);
-		if (err != nil) {
+	} else {
+		userIdInt, err := strconv.Atoi(userId)
+		if err != nil {
 			c.AbortWithError(http.StatusBadRequest, err)
 		}
 
-		if result := h.DB.Where(models.Sleep{ UserID: uint(userIdInt)}).Find(&sleeps); result.Error != nil {
+		h.DB.Model(models.Sleep{}).Where(models.Sleep{UserID: uint(userIdInt)}).Count(&pagination.totalRows)
+		if result := h.DB.Where(models.Sleep{UserID: uint(userIdInt)}).Scopes(paginator).Find(&sleeps); result.Error != nil {
 			c.AbortWithError(http.StatusNotFound, result.Error)
 			return
 		}
 	}
 
-	c.JSON(http.StatusOK, SleepsToSleepsResponse(sleeps))
+	c.JSON(http.StatusOK, gin.H{
+		"totalRows": pagination.totalRows,
+		"totalPage": pagination.getTotalPage(pagination.totalRows),
+		"rows":      SleepsToSleepsResponse(sleeps)},
+	)
 }
