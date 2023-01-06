a7525a010bbe583d6821dd0f120c63c59081ba7e
Muhammad Adnan R <madnanrizqullah@gmail.com>
2023-01-06 20:34:28 +0700

    feat: add FE prod url to deployment

--- pkg/middlewares/cors.go
+++ pkg/middlewares/cors.go
@@ -11,7 +11,7 @@ import (
 func CorsMiddleware() gin.HandlerFunc {
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowCredentials = true
-	corsConfig.AllowOrigins = []string{"http://localhost:3000", "http://localhost:3001"}
+	corsConfig.AllowOrigins = []string{"http://localhost:3000", "http://localhost:3001", "https://daily-sleep-tracker.vercel.app"}
 	corsConfig.AllowHeaders = []string{"Authorization", "Content-Type"}
 	corsConfig.AllowMethods = []string{http.MethodPost, http.MethodGet, http.MethodPut, http.MethodPatch, http.MethodDelete, http.MethodOptions}
 	return cors.New(corsConfig)
